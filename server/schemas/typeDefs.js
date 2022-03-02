a8b9f3c1d0814637f6c759996b2e36e32e299add
brandon.sauer <brantheman8@comcast.net>
2022-03-01 17:20:55 -0800

    added resolver, typeDefs and index for schema folder

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        booksCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        _id: ID
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+    type Query {
+        me: User
+        users: [User]
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    input SavedBookInput {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        forSale: String
+        link: String
+        title: String
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(book: SavedBookInput): User
+        removeBook(bookId: String!): User
+    }
+`;
+
+module.exports = typeDefs;
